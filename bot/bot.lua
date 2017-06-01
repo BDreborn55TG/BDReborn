d1c901c5829f27d15d1852efb3c7a08aee83ee5b
mhmspm <parsaye2@gmail.com>
2017-06-01 20:37:28 +0430

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -19,7 +19,7 @@ local notify = lgi.require('Notify')
 notify.init ("Telegram updates")
 chats = {}
 plugins = {}
-helper_id = 340155871 --Put Your Helper Bot ID Here
+helper_id = 387907521 --Put Your Helper Bot ID Here
 
 function do_notify (user, msg)
 	local n = notify.Notification.new(user, msg)
